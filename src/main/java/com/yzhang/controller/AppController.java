96566f014c3f06aec1bdd91924ed72a8d2b398a1
yzhang <yi_zhang2@intuit.com>
2016-12-27 19:02:23 -0800

    add service

--- src/main/java/com/yzhang/controller/AppController.java
+++ src/main/java/com/yzhang/controller/AppController.java
@@ -1,12 +1,8 @@
 package com.yzhang.controller;
 
-import com.yzhang.domain.Applicant;
-import com.yzhang.repository.ApplicantRepository;
-import com.yzhang.repository.CarRepository;
-import com.yzhang.repository.DepartmentRepository;
-import com.yzhang.repository.PersonRepository;
-
 import org.activiti.engine.RuntimeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,18 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AppController {
 
-  @Autowired
-  CarRepository carRepository;
-
-  @Autowired
-  private PersonRepository personRepository;
-
-  @Autowired
-  private DepartmentRepository departmentRepository;
-
-
-  @Autowired
-  private ApplicantRepository applicantRepository;
+  private static final Logger logger = LoggerFactory.getLogger(AppController.class);
 
   @Autowired
   private RuntimeService runtimeService;
@@ -34,8 +19,7 @@ public class AppController {
 
   @RequestMapping(value = "/jpa", method = RequestMethod.GET)
   public void testJpa() {
-    Applicant a = new Applicant("Yi", "sdfs@dsfds.com", "234232343");
-    applicantRepository.save(a);
+
     runtimeService.startProcessInstanceByKey("myProcess");
   }
 }
--- src/main/java/com/yzhang/service/AppService.java
+++ src/main/java/com/yzhang/service/AppService.java
@@ -1,14 +1,36 @@
 package com.yzhang.service;
 
+import com.yzhang.domain.Applicant;
+import com.yzhang.repository.ApplicantRepository;
+import com.yzhang.repository.CarRepository;
+import com.yzhang.repository.DepartmentRepository;
+import com.yzhang.repository.PersonRepository;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppService {
   private static final Logger logger = LoggerFactory.getLogger(AppService.class);
 
+  @Autowired
+  CarRepository carRepository;
+
+  @Autowired
+  private PersonRepository personRepository;
+
+  @Autowired
+  private DepartmentRepository departmentRepository;
+
+
+  @Autowired
+  private ApplicantRepository applicantRepository;
+
   public void startProcess() {
+    Applicant a = new Applicant("Yi", "sdfs@dsfds.com", "234232343");
+    applicantRepository.save(a);
     logger.info("in the process");
   }
 }
